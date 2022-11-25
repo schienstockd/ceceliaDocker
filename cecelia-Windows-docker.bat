4bf1e635479bc39e511f445d867e95800358b5d6
schienstockd <schienstockd@student.unimelb.edu.au>
2022-11-25 14:02:18 +1100

    misc

---
+++ cecelia-Windows-docker.bat
@@ -0,0 +1,13 @@
+@echo off
+
+:: define conda directory
+:: TODO can you get the installation directory?
+set CONDA_DIR=%UserProfile%\Miniconda3
+set CONDA_BIN=%CONDA_DIR%\condabin
+
+echo ">> START Cecelia"
+
+set CONDA_ENV=r-cecelia-gui-env
+
+:: run cecelia
+%CONDA_BIN%\conda run -n %CONDA_ENV% --no-capture-output python cecelia-docker.py
