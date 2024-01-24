cdda0154acb26f05edd8c5e715ea7e336ec871c0
schienstockd <schienstockd@student.unimelb.edu.au>
2024-01-24 16:31:31 +1100

    misc

--- cecelia-docker.py
+++ cecelia-docker.py
@@ -20,85 +20,87 @@ shiny_url = 'http://localhost:6860'
 
 # start ipython kernel detached
 def start_ipython(conn_file):
-    print('>> Start IPython')
+  print('>> Start IPython')
 
-    IPython.start_kernel(argv=[f'-f={conn_file}'])
+  IPython.start_kernel(argv=[f'-f={conn_file}'])
 
 # start ipython kernel detached
 def start_docker(datashare_path):
-    print('>> Start Docker')
+  print('>> Start Docker')
 
-    if platform.system() != 'Windows':
-        os.system(f'export JUPYTER_LIB_DIR="{datashare_path}" && docker compose up --build --remove-orphans')
-    else:
-        # escape backslashes for R
-        datashare_path = datashare_path.replace('\\', '\\\\')
-        os.system(f'set JUPYTER_LIB_DIR={datashare_path} && docker compose up --build --remove-orphans')
+  # Docker compose is a bit complicated
+  # https://github.com/docker/compose/issues/1487
+  if platform.system() != 'Windows':
+    os.system(f'export JUPYTER_LIB_DIR="{datashare_path}" && docker compose up --build -V --remove-orphans')
+  else:
+    # escape backslashes for R
+    datashare_path = datashare_path.replace('\\', '\\\\')
+    os.system(f'set JUPYTER_LIB_DIR={datashare_path} && docker compose up --build -V --remove-orphans')
 
 if __name__ == '__main__':
-    if os.path.exists(datashare_path) is False:
-        os.makedirs(datashare_path)
+  if os.path.exists(datashare_path) is False:
+    os.makedirs(datashare_path)
 
-    print('>> Remove connection files')
+  print('>> Remove connection files')
 
-    # remove connection file for IPython kernel
+  # remove connection file for IPython kernel
+  if os.path.exists(debug_file):
+    os.remove(debug_file)
+  if os.path.exists(conn_file):
+    os.remove(conn_file)
+
+  # start ipython
+  p1 = Process(target = start_ipython, args = (debug_file,))
+  p1.start()
+
+  # wait until jupyter started and replace for docker
+  ipython_init = False
+
+  print('>> wait for kernel...')
+
+  while ipython_init is False:
     if os.path.exists(debug_file):
-        os.remove(debug_file)
-    if os.path.exists(conn_file):
-        os.remove(conn_file)
+      ipython_init = True
+    else:
+      time.sleep(0.5)
 
-    # start ipython
-    p1 = Process(target = start_ipython, args = (debug_file,))
-    p1.start()
+  print('>> OK')
 
-    # wait until jupyter started and replace for docker
-    ipython_init = False
+  # replace host
+  with open(debug_file, 'r') as file:
+    data = file.read()
+    data = data.replace('127.0.0.1', 'host.docker.internal')
 
-    print('>> wait for kernel...')
+  # write back
+  with open(conn_file, 'w') as file:
+    file.write(data)
+    file.close()
 
-    while ipython_init is False:
-        if os.path.exists(debug_file):
-            ipython_init = True
-        else:
-            time.sleep(0.5)
+  # start docker
+  p2 = Process(target = start_docker, args = (datashare_path,))
+  p2.start()
 
-    print('>> OK')
+  # wait for shiny to start up
+  docker_init = False
 
-    # replace host
-    with open(debug_file, 'r') as file:
-        data = file.read()
-        data = data.replace('127.0.0.1', 'host.docker.internal')
+  print('>> wait for docker...')
 
-    # write back
-    with open(conn_file, 'w') as file:
-        file.write(data)
-        file.close()
+  while docker_init is False:
+    try:
+      ret_code = urllib.request.urlopen(shiny_url).getcode()
 
-    # start docker
-    p2 = Process(target = start_docker, args = (datashare_path,))
-    p2.start()
+      if ret_code == 200:
+        docker_init = True
+      else:
+        time.sleep(0.5)
+    except:
+      time.sleep(1)
 
-    # wait for shiny to start up
-    docker_init = False
+  print('>> OK')
+  webbrowser.open(shiny_url)
 
-    print('>> wait for docker...')
+  # wait for docker exit
+  p2.join()
 
-    while docker_init is False:
-        try:
-            ret_code = urllib.request.urlopen(shiny_url).getcode()
-
-            if ret_code == 200:
-                docker_init = True
-            else:
-                time.sleep(0.5)
-        except:
-            time.sleep(1)
-
-    print('>> OK')
-    webbrowser.open(shiny_url)
-
-    # wait for docker exit
-    p2.join()
-
-    # kill jupyter
-    p1.terminate()
+  # kill jupyter
+  p1.terminate()
